4308f6ae147fb2f53cba769ac9eb668f254c5071
Avi Lance <avi.lance@speen.org>
2023-10-11 22:30:30 -0700

    Added CustomMappingTransformer

--- library.py
+++ library.py
@@ -5,6 +5,51 @@ sklearn.set_config(transform_output="pandas")  #pass pandas tables through pipel
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 
+class CustomMappingTransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, mapping_column, mapping_dict:dict):
+    assert isinstance(mapping_dict, dict), f'{self.__class__.__name__} constructor expected dictionary but got {type(mapping_dict)} instead.'
+    self.mapping_dict = mapping_dict
+    self.mapping_column = mapping_column
+
+  def fit(self, X, y = None):
+    print(f"\nWarning: {self.__class__.__name__}.fit does nothing.\n")
+    return self
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'{self.__class__.__name__}.transform expected Dataframe but got {type(X)} instead.'
+    assert self.mapping_column in X.columns.to_list(), f'{self.__class__.__name__}.transform unknown column "{self.mapping_column}"'  #column legit?
+
+    #Set up for producing warnings. First have to rework nan values to allow set operations to work.
+    #In particular, the conversion of a column to a Series, e.g., X[self.mapping_column], transforms nan values in strange ways that screw up set differencing.
+    #Strategy is to convert empty values to a string then the string back to np.nan
+    placeholder = "NaN"
+    column_values = X[self.mapping_column].fillna(placeholder).tolist()  #Convert all nan values to the string "NaN" in new list
+    column_values = [np.nan if v == placeholder else v for v in column_values]  #Now convert back to np.nan
+    keys_values = self.mapping_dict.keys()
+
+    column_set = set(column_values)  #Without the conversion above, the set will fail to have np.nan values where they should be
+    keys_set = set(keys_values)      #This will have np.nan values where they should be so no conversion necessary
+
+    #Verify all keys are contained in column
+    keys_not_found = keys_set - column_set
+    if keys_not_found:
+      print(f"\nWarning: {self.__class__.__name__}[{self.mapping_column}] these mapping keys do not appear in the column: {keys_not_found}\n")
+
+    #Verify if keys are absent
+    keys_absent = column_set -  keys_set
+    if keys_absent:
+      print(f"\nWarning: {self.__class__.__name__}[{self.mapping_column}] these values in the column do not contain corresponding mapping keys: {keys_absent}\n")
+
+    #Actual mapping
+    X_ = X.copy()
+    X_[self.mapping_column].replace(self.mapping_dict, inplace=True)
+    return X_
+
+  def fit_transform(self, X, y = None):
+    #self.fit(X,y)
+    result = self.transform(X)
+    return result
+
 #This class will rename one or more columns.
 class CustomRenamingTransformer(BaseEstimator, TransformerMixin):
   def __init__(self, rename_dict:dict):
@@ -33,9 +78,6 @@ class CustomRenamingTransformer(BaseEstimator, TransformerMixin):
 #This class will perform a One Hot Encoding
 class CustomOHETransformer(BaseEstimator, TransformerMixin):
   def __init__(self, target_column, dummy_na=False, drop_first=False):
-    assert isinstance(target_column, str), f'{self.__class__.__name__} constructor expected str for target_column but got {type(target_column)} instead.'
-    assert isinstance(dummy_na, bool), f'{self.__class__.__name__} constructor expected boolean for dummy_na but got {type(dummy_na)} instead.'
-    assert isinstance(drop_first, bool), f'{self.__class__.__name__} constructor expected boolean for drop_first but got {type(drop_first)} instead.'
     self.target_column = target_column
     self.dummy_na = dummy_na
     self.drop_first = drop_first
@@ -62,4 +104,3 @@ class CustomOHETransformer(BaseEstimator, TransformerMixin):
     #self.fit(X,y)
     result = self.transform(X)
     return result
-
